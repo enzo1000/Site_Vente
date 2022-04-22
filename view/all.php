4db9c473adcf08adb4d7036307e38f036365e772
BURILLE <90316492+BURILLE@users.noreply.github.com>
2022-04-22 16:39:24 +0200

    les images marche je vais taper enzo

--- view/all.php
+++ view/all.php
@@ -8,15 +8,22 @@ session_start();
     <link rel="stylesheet" href="CSS/index.css">
     <title>Jeu de <?php
         if(isset($_SESSION['pseudo'])) {echo $_SESSION['pseudo'];}
-        else echo "Guest"?></title>
+        else echo "Guest";?></title>
 </head>
 <body>
 
 <?php
-foreach ($tab_p as $produit)
-    echo '<p> Livre n°'. $produit->getId() . '</p>'
-        . "<p>" . $produit->getNom() .
-            $produit->getDescription() .'</p>';
+foreach ($tab_p as $produit) {
+
+
+    echo "<img src='view/images/".$produit->getPhoto()."/1.png' class='image' alt=" . "texte alternatif" . " title=" . "Titre de l'image/>"
+
+        . "<div class='titre'>" . $produit->getNom() . "</div>"
+        . $produit->getDescription() . '</p>';
+
+}
+
+//<p> Livre n°'. $produit->getId() . '</p>'
 ?>
 
 </body>
