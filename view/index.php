113b0f14dcb359b471ede5414c68409271bb09d1
Enzo Martinez <35664442+enzo1000@users.noreply.github.com>
2022-04-22 13:46:46 +0200

    Structure de base

--- view/index.php
+++ view/index.php
@@ -1,1 +1,19 @@
 <?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/index.css">
+    <title>Jeu de <?php
+        if(isset($_SESSION['pseudo'])) {echo $_SESSION['pseudo'];}
+        else echo "Guest"?></title>
+</head>
+<body>
+
+
+
+</body>
+
+</html>
