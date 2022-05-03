d1da236b256487abb23ef34007e6323ab11989e3
BURILLE <90316492+BURILLE@users.noreply.github.com>
2022-05-03 15:29:50 +0200

    ajout boutton like

--- view/all.php
+++ view/all.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <!--<link rel="stylesheet" href="<?php //echo File::build_path(array("view","CSS","index.css"))?>"-->
     <link rel="stylesheet" href="view/CSS/index.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@3.0.0/dist/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">-->
 
     <title>Jeu de <?php
         if (isset($_SESSION['pseudo'])) {
@@ -28,8 +28,11 @@ foreach ($tab_p as $produit) {
         . "</div>"
         . "<div class='prix'>" . $produit->getPrix() . " €</div>"
         . "<div class='description'>" . $produit->getDescription() . "</div>"
+        /*
         . "<button type='button' class='btn btn-info btn-circle btn-lg'><i class='glyphicon glyphicon-ok'></i></button>"
         . "<button type='button' class='btn btn-danger btn-circle btn-lg'><i class='glyphicon glyphicon-heart'></i></button>"
+        */
+        ."<button class='like'></button>"
         ."</div>"
         ."</div>";
 
